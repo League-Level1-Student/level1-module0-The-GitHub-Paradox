b14b55c2a9467dcff06be0e6c4c130d1c32cc385
Meme1sh <60296686+The-GitHub-Paradox@users.noreply.github.com>
2020-07-11 20:09:28 -0700

    e
    e

--- src/_04_drum_kit/DrumKit.java
+++ src/_04_drum_kit/DrumKit.java
@@ -42,20 +42,19 @@ public class DrumKit implements MouseListener {
 		// Eclipse project under "default package".
 
 		// 8. Put the name of your image file in a String variable.
-String s = "snare.jpg";
+	String s = "snare.jpg";
 		// 9. Edit the next line to use your String variable
 		// drumLabelWithImage = createLabelImage(drumImageString);
-drumLabelWithImage = createLabelImage(s);
+	drumLabelWithImage = createLabelImage(s);
 		// 10. Add the image to the panel
-p.add(drumLabelWithImage);
+	p.add(drumLabelWithImage);
 		// 11. Set the layout of the panel to "new GridLayout()"
-layout p = new GridLayout();
+	new GridLayout();
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
-f.pack();
-
+	f.pack();
 		// 13. add this mouse listener to drumLabelWithImage
-
+	
 		// 18. Add more images to make a drumkit. Remember to add this mouse
 		// listener to each one.
 
--- src/_05_body_part_quiz/BodyPartQuiz.java
+++ src/_05_body_part_quiz/BodyPartQuiz.java
@@ -51,13 +51,21 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+		int score = 0;
 		// 2. Set the size of the window in the initializeGui() method 
-
+		initializeGui();
 		// 4. Ask the user who this person is and store their answer
 		String guess = JOptionPane.showInputDialog("who is this?");
 
 		// 5. Check their answer. If they guessed correctly:
+		if(guess.contentEquals("Arnold")) {
+			System.out.println("You got it right!");
+			score++;
+			showNextImage();
+		}else {
+			System.out.println("You got it wrong. Art thou uncultured swine!");
+			showNextImage();
+		}
 		// -- Tell them they are right and increase the score by 1
 
 		// 6. Otherwise:
