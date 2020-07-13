9c361faf292e53ca93a8cbd9bbb65f67658e0584
Meme1sh <60296686+The-GitHub-Paradox@users.noreply.github.com>
2020-07-13 14:02:04 -0700

    e
    e

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -18,7 +18,7 @@ public class FortuneCookie implements ActionListener {
 		f.add(button);
 		f.pack();
 		button.addActionListener(this);
-		int rand = new Random().nextInt(5);
+		int rand = new Random().nextInt(6);
 		if(rand == 0 ) {
 			JOptionPane.showInputDialog("You will be ran over by a car lmao bad");
 		}
@@ -30,6 +30,15 @@ public class FortuneCookie implements ActionListener {
 		}
 		if(rand == 3) {
 			JOptionPane.showInputDialog("Your next haircut will consist of a lawnmower and a camel");
+		}
+		if(rand == 4) {
+			JOptionPane.showInputDialog("Your first friend will challenge you to a duel and fatally shoot you in the ribs.");
+		}
+		if(rand == 5) {
+			JOptionPane.showInputDialog("Look behind you");
+		}
+		if(rand == 6) {
+			JOptionPane.showInputDialog("You will be the richest person alive soon. The richest person in terms of nothing..");
 		}
 	}
 	
